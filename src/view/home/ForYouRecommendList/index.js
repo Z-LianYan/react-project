65e167091479f50edd83caa04d70f1b923c72d3d
Z-LianYan <1371935318@qq.com>
2020-07-27 18:32:35 +0800

    1

--- src/view/home/ForYouRecommendList/index.js
+++ src/view/home/ForYouRecommendList/index.js
@@ -1,6 +1,10 @@
 import React,{ Component } from "react";
 import "./index.scss";
 
+import { 
+    GET_RECOMMEND_LIST
+} from "@/api/home";
+
 export default class ForYouRecommendList extends Component{
     constructor(props){//构造函数，最先被执行,通常在构造函数里初始化state对象或者给自定义方法绑定this
         // console.log("构造函数，最先被执行")
@@ -13,19 +17,30 @@ export default class ForYouRecommendList extends Component{
 
     static getDerivedStateFromProps(nextProps, prevState){//挂载更新都会执行 (必须返回一个有效的状态对象(或null))
         // console.log("getDerivedStateFromProps 是个静态方法,当我们接收到新的属性想去修改我们state")
+
+        console.log("nextProps-=",nextProps,"prevState-=",prevState);
+
+        if(nextProps.forYourecommendList.length){
+            return {
+                forYourecommendList:nextProps.forYourecommendList
+            }
+        }
+
+
+
         return null;
     }
 
 
     render(){
         // console.log("render函数是纯函数===",this.props);
-        const { forYourecommendList } = this.props;
+        // const { forYourecommendList } = this.props;
         
 
         return (
             <section className="for-you-recommend-list padding-rl clear" ref={ref=>this.newPinterest = ref}>
                 <ul className="item1">
-                    { forYourecommendList.length && forYourecommendList.map((item,index)=>{
+                    { this.state.forYourecommendListLeft.length && this.state.forYourecommendListLeft.map((item,index)=>{
                         return (
                             <a href="/" className="item-wrapper" key={index}>
                                 <div className="img-wrapper">
@@ -63,12 +78,12 @@ export default class ForYouRecommendList extends Component{
                     })}
                 </ul>
                 <ul className="item2">
-                    { forYourecommendList.length && forYourecommendList.map((item,index)=>{
+                    { this.state.forYourecommendListRight.length && this.state.forYourecommendListRight.map((item,index)=>{
                         return (
                             <a href="/" className="item-wrapper" key={index}>
                                 <div className="img-wrapper">
                                     <img src={item.pic} alt=""/>
-                                    <span className="addr">广州</span>
+                                    <span className="addr">{item.city_name}</span>
                                 </div>
                                 
                                 <div className="for-you-commend-content">
@@ -110,7 +125,7 @@ export default class ForYouRecommendList extends Component{
         const {newPinterest} = this;
         const {childNodes} = newPinterest;
         let minData = childNodes[0];
-        childNodes&&childNodes.forEach(item=>{
+        childNodes && childNodes.forEach(item=>{
             if(item.offsetHeight<minData.offsetHeight){
                 minData = item
             }
@@ -121,11 +136,58 @@ export default class ForYouRecommendList extends Component{
 
 
     componentDidMount(){
-        console.log("componentDidMount 组件装载之后调用")
-        
+        console.log("componentDidMount 组件装载之后调用");
+        this.fetchRecommendList()
+    }
 
+    getData(){
+        console.log("获取数据")
+    }
+
+
+    async fetchRecommendList(){
+        const result = await GET_RECOMMEND_LIST({
+            city_id: 0,
+            category: "",
+            keywords: "",
+            venue_id: "",
+            start_time: "",
+            page: 1,
+            referer_type: "index"
+        });
+
+        // console.log("结果----====---=-=-=",result);
+
+        // const { forYourecommendList } = this.props;
+        const {  forYourecommendListLeft,forYourecommendListRight } = this.state;
+
+
+        result.list.forEach(item=>{
+
+            const a = this.getMinContain();
+        
+            switch (a.className){
+                case 'item1':forYourecommendListLeft.push(item);break;
+                case 'item2':forYourecommendListRight.push(item);break;
+                default:return null;
+            }
+            this.setState({forYourecommendListLeft,forYourecommendListRight});
+        })
+
+        console.log("婚纱哈哈哈哈哈哈",this.state.forYourecommendListLeft,this.state.forYourecommendListRight)
+
+
+
+        this.setState({
+            forYourecommendList:result.list
+        })
+
+        console.log("为你推荐列表",result,this.state.forYourecommendList);
 
     }
+
+
+
 
 
 
@@ -151,11 +213,12 @@ export default class ForYouRecommendList extends Component{
 
 
 
-        
+
 
         // const { forYourecommendList } = this.props;
         // const {  forYourecommendListLeft,forYourecommendListRight } = this.state;
         // forYourecommendList.map(item=>{
+
         //     const a = this.getMinContain();
         
         //     switch (a.className){
